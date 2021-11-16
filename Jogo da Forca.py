e2679ac8b84823cb0627add8a042da6b4be0dbb8
Luan Ramalho <32373552+LuanRamalho@users.noreply.github.com>
2021-11-16 20:13:20 -0300

    Update Jogo da Forca.py

--- Jogo da Forca.py
+++ Jogo da Forca.py
@@ -1,12 +1,12 @@
 #This game has pre-specified input
 import time
-name = input("What is your name? ")
-print("Hello, " + name, "Time to play hangman!")
+name = input("Qual é o seu nome? ")
+print("Olá, " + name, "Hora de jogar forca!")
 print("")
 time.sleep(1)
-print("Start guessing...")
+print("Comece a adivinhar...")
 time.sleep(0.5)
-word = "secret"
+word = "secreto"
 guesses = ''
 turns = 10
 while turns > 0:
@@ -18,14 +18,14 @@ while turns > 0:
             print("_")
             failed += 1
     if failed == 0:
-        print("You won")
+        print("Você ganhou")
         break
     print('')
-    guess = input("guess a character:")
+    guess = input("adivinhe um personagem:")
     guesses += guess
     if guess not in word:
         turns -= 1
-        print("Wrong")
-    print("You have", + turns, 'more guesses')
+        print("Errado")
+    print("Você tem", + turns, 'mais suposições')
     if turns == 0:
-        print("You Lose")
+        print("Você perdeu")
